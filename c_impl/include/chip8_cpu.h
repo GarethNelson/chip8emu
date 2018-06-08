3226869730c8ea55f78fbf489720676d38a7004a
Gareth Nelson <gareth@garethnelson.com>
2018-06-08 04:43:03 +0100

    all instructions in place, start on sprite rendering

--- c_impl/include/chip8_cpu.h
+++ c_impl/include/chip8_cpu.h
@@ -2,6 +2,9 @@
 
 #include <stdint.h>
 
+#define CPU8_STACK_START 0x0EA0
+#define CPU8_VRAM_START  0x0F00
+
 typedef struct chip8_reg_t {
 	uint8_t  V[16];
 	uint16_t I;
@@ -14,10 +17,11 @@ typedef struct chip8_reg_t {
 typedef struct chip8_cpu_t {
 	chip8_reg_t registers;
 	uint8_t     ram[4096];
-	uint16_t    stack[16];
+	uint8_t     *vram;
 } chip8_cpu_t;
 
 chip8_cpu_t* chip8_new_cpu();
 void chip8_load_rom(chip8_cpu_t* cpu, char* filename);
 void chip8_dump_status(chip8_cpu_t* cpu);
 const char* chip8_disasm(chip8_cpu_t* cpu, uint16_t offset);
+void chip8_iter(chip8_cpu_t* cpu);
--- c_impl/src/chip8_cpu.c
+++ c_impl/src/chip8_cpu.c
@@ -11,15 +11,15 @@ chip8_cpu_t* chip8_new_cpu() {
 	// yes, calloc is meant to zero stuff out, some platforms don't and i want this code portable, for reasons
 	memset(retval->registers.V,'\0',16);
 
-	memset(retval->stack,'\0',16);
 	memset(retval->ram,  '\0',4096);
 	retval->registers.I  = 0;
 	retval->registers.PC = 0x200; // where programs start by default
 	retval->registers.DT = 0;
 	retval->registers.ST = 0;
-	retval->registers.SP = 0xEA0;
+	retval->registers.SP = CPU8_STACK_START;
 
 	memcpy(retval->ram, chip8_rom_font, 80);
+	retval->vram = &(retval->ram[CPU8_VRAM_START]);
 	return retval;
 }
 
@@ -223,6 +223,198 @@ const char* chip8_disasm(chip8_cpu_t* cpu, uint16_t offset) {
 
      return retval;
 }
+
+void chip8_iter(chip8_cpu_t* cpu) {
+     static char retval[32];
+     snprintf(retval,32,"%s","UNKNOWN");
+     uint16_t offset = cpu->registers.PC;
+     uint8_t* code = &(cpu->ram[offset]);
+     uint8_t upper_nibble = UPPER_NIBBLE_U8(code[0]);
+     uint8_t lower_nibble = LOWER_NIBBLE_U8(code[0]);
+     switch(upper_nibble) {
+	case 0x00: {
+	     switch(code[1]) {
+		 case 0x00: {
+		       snprintf(retval,32,"%04x %02x %02x %-10s", offset, code[0], code[1], "NOP");
+		       break;
+		 }
+                 case 0xE0: {
+		       snprintf(retval,32,"%04x %02x %02x %-10s", offset, code[0], code[1], "CLS");
+		       break;
+	         }
+	         case 0xEE: {
+ 		       snprintf(retval,32,"%04x %02x %02x %-10s",offset, code[0], code[1], "RTS");
+		       break;
+
+	         }
+                 default: {
+		   snprintf(retval,32,"%04x %02x %02x %-10s", offset, code[0], code[1], "UNKNOWN");
+		   break;
+	 	 }
+	     }
+	     break;
+	}
+	case 0x01: {
+	     snprintf(retval,32,"%04x %02x %02x %-10s %02x%02x", offset, code[0], code[1], "JMP",lower_nibble,code[1]);
+	     break;
+	}
+	case 0x02: {
+	     snprintf(retval,32,"%04x %02x %02x %-10s %02x%02x", offset, code[0], code[1], "CALL",lower_nibble,code[1]);
+	     break;
+	}
+	case 0x03: {
+	     snprintf(retval,32,"%04x %02x %02x %-10s V%01x %02x", offset, code[0], code[1], "SKIP.EQ",lower_nibble,code[1]);
+	     break;
+	}
+	case 0x04: {
+	     snprintf(retval,32,"%04x %02x %02x %-10s V%01x %02x", offset, code[0], code[1], "SKIP.NE",lower_nibble,code[1]);
+	     break;
+	}
+
+   	case 0x05: {
+	     snprintf(retval,32,"%04x %02x %02x %-10s V%01x V%01x", offset, code[0], code[1], "SKIP.EQ",lower_nibble,UPPER_NIBBLE_U8(code[1]));
+	     break;
+	}
+	case 0x09: {
+	     snprintf(retval,32,"%04x %02x %02x %-10s V%01x V%01x", offset, code[0], code[1], "SKIP.NE",lower_nibble,UPPER_NIBBLE_U8(code[1]));
+	     break;
+	}
+
+	case 0x0B: {
+	     snprintf(retval,32,"%04x %02x %02x %-10s V0(%02x%02x)", offset, code[0], code[1], "JMP",lower_nibble,code[1]);
+	     break;
+  	}
+
+	case 0x06: {
+             snprintf(retval,32,"%04x %02x %02x %-10s V%01x,#$%02x", offset, code[0], code[1], "MOV IMM", code[0] & 0x0f, code[1]);
+	     break;
+	}
+	case 0x08: {
+	     switch(LOWER_NIBBLE_U8(code[1])) {
+		case 0x00: {
+		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x,V%01x", offset, code[0], code[1], "MOV", lower_nibble, UPPER_NIBBLE_U8(code[1]));
+		     break;
+		}
+		case 0x01: {
+		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x,V%01x", offset, code[0], code[1], "OR", lower_nibble, UPPER_NIBBLE_U8(code[1]));
+		     break;
+		}
+		case 0x02: {
+		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x,V%01x", offset, code[0], code[1], "AND", lower_nibble, UPPER_NIBBLE_U8(code[1]));
+		     break;
+		}
+		case 0x03: {
+		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x,V%01x", offset, code[0], code[1], "XOR", lower_nibble, UPPER_NIBBLE_U8(code[1]));
+		     break;
+		}
+		case 0x04: {
+		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x,V%01x", offset, code[0], code[1], "ADD.", lower_nibble, UPPER_NIBBLE_U8(code[1]));
+		     break;
+		}
+		case 0x05: {
+		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x,V%01x", offset, code[0], code[1], "SUB.", lower_nibble, UPPER_NIBBLE_U8(code[1]));
+		     break;
+		}
+		case 0x06: {
+		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x", offset, code[0], code[1], "SHR.", lower_nibble);
+		     break;
+		}
+		case 0x07: {
+		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x,V%01x", offset, code[0], code[1], "SUBB.", lower_nibble, UPPER_NIBBLE_U8(code[1]));
+		     break;
+		}
+		case 0x0E: {
+		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x", offset, code[0], code[1], "SHL.", lower_nibble);
+		     break;
+		}
+	        default: {
+		   snprintf(retval,32,"%04x %02x %02x %-10s", offset, code[0], code[1], "UNKNOWN");
+		   break;
+	 	}
+	     }
+	     break;
+	}
+	case 0x0A: {
+             snprintf(retval,32,"%04x %02x %02x %-10s I,#$%01x%02x", offset, code[0], code[1], "MOV IMM", code[0] & 0x0f, code[1]);
+	     break;
+	}
+	case 0x0D: {
+	     snprintf(retval,32,"%04x %02x %02x %-10s V%01x,V%01x #$%01x", offset, code[0], code[1], "SPRITE", lower_nibble, UPPER_NIBBLE_U8(code[1]),LOWER_NIBBLE_U8(code[1]));	     
+	     break;
+	}
+	case 0xE: {
+	     switch(code[1]) {
+		case 0x9E:{
+		     snprintf(retval,32,"%04x %02x %02x %-10s",offset, code[0], code[1], "SKIP.KEY");
+	             break;
+	        }
+
+		case 0xA1:{
+		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x",offset, code[0], code[1], "SKIP.NOKEY",lower_nibble);
+	             break;
+	        }
+                default: {
+		   snprintf(retval,32,"%04x %02x %02x %-10s", offset, code[0], code[1], "UNKNOWN");
+		   break;
+	 	}
+	     }
+	     break;
+	}
+	case 0xF:{
+	     switch(code[1]) {
+		case 0x07: {
+		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x DELAY", offset, code[0], code[1], "MOV", lower_nibble);
+	             break;
+		}
+		case 0x0A: {
+		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x",offset, code[0], code[1], "WAITKEY",lower_nibble);
+	             break;
+	        }
+		case 0x15: {
+		     snprintf(retval,32,"%04x %02x %02x %-10s DELAY V%01x", offset, code[0], code[1], "MOV", lower_nibble);
+	             break;
+ 		}
+		case 0x18: {
+		     snprintf(retval,32,"%04x %02x %02x %-10s SOUND V%01x", offset, code[0], code[1], "MOV", lower_nibble);
+	             break;
+ 		}
+		case 0x1E: {
+                     snprintf(retval,32,"%04x %02x %02x %-10s I V%01x", offset, code[0], code[1], "ADD", lower_nibble);
+	             break;
+	        }
+		case 0x29: {
+                     snprintf(retval,32,"%04x %02x %02x %-10s V%01x", offset, code[0], code[1], "SPRITECHAR", lower_nibble);
+	             break;
+ 		}
+		case 0x33: {
+                     snprintf(retval,32,"%04x %02x %02x %-10s V%01x", offset, code[0], code[1], "MOVBCD", lower_nibble);
+	             break;
+	        }
+		case 0x55: {
+                     snprintf(retval,32,"%04x %02x %02x %-10s (I), V0-V%01x", offset, code[0], code[1], "MOVM", lower_nibble);
+	             break;
+	        }
+		case 0x65: {
+                     snprintf(retval,32,"%04x %02x %02x %-10s V0-V%01x, (I)", offset, code[0], code[1], "MOVM", lower_nibble);
+	             break;
+	        }
+		default: {
+		   snprintf(retval,32,"%04x %02x %02x %-10s", offset, code[0], code[1], "UNKNOWN");
+		   break;
+	 	}
+	     }
+	     break;
+	}
+
+
+	default: {
+             snprintf(retval,32,"%04x %02x %02x %-10s", offset, code[0], code[1], "UNKNOWN");
+	}
+     }
+
+
+}
+
 
 void chip8_dump_status(chip8_cpu_t* cpu) {
      printf("V:  %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x \n", 
