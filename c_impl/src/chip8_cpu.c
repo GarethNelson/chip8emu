da9b2b350eea08b4c23cc5e22ee165d827dc78ed
Gareth Nelson <gareth@garethnelson.com>
2018-06-08 16:29:16 +0100

    start of actual emulator

--- c_impl/src/chip8_cpu.c
+++ c_impl/src/chip8_cpu.c
@@ -224,195 +224,43 @@ const char* chip8_disasm(chip8_cpu_t* cpu, uint16_t offset) {
      return retval;
 }
 
-void chip8_iter(chip8_cpu_t* cpu) {
-     static char retval[32];
-     snprintf(retval,32,"%s","UNKNOWN");
-     uint16_t offset = cpu->registers.PC;
-     uint8_t* code = &(cpu->ram[offset]);
-     uint8_t upper_nibble = UPPER_NIBBLE_U8(code[0]);
-     uint8_t lower_nibble = LOWER_NIBBLE_U8(code[0]);
-     switch(upper_nibble) {
-	case 0x00: {
-	     switch(code[1]) {
-		 case 0x00: {
-		       snprintf(retval,32,"%04x %02x %02x %-10s", offset, code[0], code[1], "NOP");
-		       break;
-		 }
-                 case 0xE0: {
-		       snprintf(retval,32,"%04x %02x %02x %-10s", offset, code[0], code[1], "CLS");
-		       break;
-	         }
-	         case 0xEE: {
- 		       snprintf(retval,32,"%04x %02x %02x %-10s",offset, code[0], code[1], "RTS");
-		       break;
-
-	         }
-                 default: {
-		   snprintf(retval,32,"%04x %02x %02x %-10s", offset, code[0], code[1], "UNKNOWN");
-		   break;
-	 	 }
-	     }
-	     break;
-	}
-	case 0x01: {
-	     snprintf(retval,32,"%04x %02x %02x %-10s %02x%02x", offset, code[0], code[1], "JMP",lower_nibble,code[1]);
-	     break;
-	}
-	case 0x02: {
-	     snprintf(retval,32,"%04x %02x %02x %-10s %02x%02x", offset, code[0], code[1], "CALL",lower_nibble,code[1]);
-	     break;
-	}
-	case 0x03: {
-	     snprintf(retval,32,"%04x %02x %02x %-10s V%01x %02x", offset, code[0], code[1], "SKIP.EQ",lower_nibble,code[1]);
-	     break;
-	}
-	case 0x04: {
-	     snprintf(retval,32,"%04x %02x %02x %-10s V%01x %02x", offset, code[0], code[1], "SKIP.NE",lower_nibble,code[1]);
-	     break;
-	}
-
-   	case 0x05: {
-	     snprintf(retval,32,"%04x %02x %02x %-10s V%01x V%01x", offset, code[0], code[1], "SKIP.EQ",lower_nibble,UPPER_NIBBLE_U8(code[1]));
-	     break;
-	}
-	case 0x09: {
-	     snprintf(retval,32,"%04x %02x %02x %-10s V%01x V%01x", offset, code[0], code[1], "SKIP.NE",lower_nibble,UPPER_NIBBLE_U8(code[1]));
-	     break;
-	}
-
-	case 0x0B: {
-	     snprintf(retval,32,"%04x %02x %02x %-10s V0(%02x%02x)", offset, code[0], code[1], "JMP",lower_nibble,code[1]);
-	     break;
-  	}
-
-	case 0x06: {
-             snprintf(retval,32,"%04x %02x %02x %-10s V%01x,#$%02x", offset, code[0], code[1], "MOV IMM", code[0] & 0x0f, code[1]);
-	     break;
-	}
-	case 0x08: {
-	     switch(LOWER_NIBBLE_U8(code[1])) {
-		case 0x00: {
-		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x,V%01x", offset, code[0], code[1], "MOV", lower_nibble, UPPER_NIBBLE_U8(code[1]));
-		     break;
-		}
-		case 0x01: {
-		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x,V%01x", offset, code[0], code[1], "OR", lower_nibble, UPPER_NIBBLE_U8(code[1]));
-		     break;
-		}
-		case 0x02: {
-		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x,V%01x", offset, code[0], code[1], "AND", lower_nibble, UPPER_NIBBLE_U8(code[1]));
-		     break;
-		}
-		case 0x03: {
-		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x,V%01x", offset, code[0], code[1], "XOR", lower_nibble, UPPER_NIBBLE_U8(code[1]));
-		     break;
-		}
-		case 0x04: {
-		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x,V%01x", offset, code[0], code[1], "ADD.", lower_nibble, UPPER_NIBBLE_U8(code[1]));
-		     break;
-		}
-		case 0x05: {
-		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x,V%01x", offset, code[0], code[1], "SUB.", lower_nibble, UPPER_NIBBLE_U8(code[1]));
-		     break;
-		}
-		case 0x06: {
-		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x", offset, code[0], code[1], "SHR.", lower_nibble);
-		     break;
-		}
-		case 0x07: {
-		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x,V%01x", offset, code[0], code[1], "SUBB.", lower_nibble, UPPER_NIBBLE_U8(code[1]));
-		     break;
-		}
-		case 0x0E: {
-		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x", offset, code[0], code[1], "SHL.", lower_nibble);
-		     break;
-		}
-	        default: {
-		   snprintf(retval,32,"%04x %02x %02x %-10s", offset, code[0], code[1], "UNKNOWN");
-		   break;
-	 	}
-	     }
-	     break;
-	}
-	case 0x0A: {
-             snprintf(retval,32,"%04x %02x %02x %-10s I,#$%01x%02x", offset, code[0], code[1], "MOV IMM", code[0] & 0x0f, code[1]);
-	     break;
-	}
-	case 0x0D: {
-	     snprintf(retval,32,"%04x %02x %02x %-10s V%01x,V%01x #$%01x", offset, code[0], code[1], "SPRITE", lower_nibble, UPPER_NIBBLE_U8(code[1]),LOWER_NIBBLE_U8(code[1]));	     
-	     break;
-	}
-	case 0xE: {
-	     switch(code[1]) {
-		case 0x9E:{
-		     snprintf(retval,32,"%04x %02x %02x %-10s",offset, code[0], code[1], "SKIP.KEY");
-	             break;
-	        }
-
-		case 0xA1:{
-		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x",offset, code[0], code[1], "SKIP.NOKEY",lower_nibble);
-	             break;
-	        }
-                default: {
-		   snprintf(retval,32,"%04x %02x %02x %-10s", offset, code[0], code[1], "UNKNOWN");
-		   break;
-	 	}
-	     }
-	     break;
-	}
-	case 0xF:{
-	     switch(code[1]) {
-		case 0x07: {
-		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x DELAY", offset, code[0], code[1], "MOV", lower_nibble);
-	             break;
-		}
-		case 0x0A: {
-		     snprintf(retval,32,"%04x %02x %02x %-10s V%01x",offset, code[0], code[1], "WAITKEY",lower_nibble);
-	             break;
-	        }
-		case 0x15: {
-		     snprintf(retval,32,"%04x %02x %02x %-10s DELAY V%01x", offset, code[0], code[1], "MOV", lower_nibble);
-	             break;
- 		}
-		case 0x18: {
-		     snprintf(retval,32,"%04x %02x %02x %-10s SOUND V%01x", offset, code[0], code[1], "MOV", lower_nibble);
-	             break;
- 		}
-		case 0x1E: {
-                     snprintf(retval,32,"%04x %02x %02x %-10s I V%01x", offset, code[0], code[1], "ADD", lower_nibble);
-	             break;
-	        }
-		case 0x29: {
-                     snprintf(retval,32,"%04x %02x %02x %-10s V%01x", offset, code[0], code[1], "SPRITECHAR", lower_nibble);
-	             break;
- 		}
-		case 0x33: {
-                     snprintf(retval,32,"%04x %02x %02x %-10s V%01x", offset, code[0], code[1], "MOVBCD", lower_nibble);
-	             break;
-	        }
-		case 0x55: {
-                     snprintf(retval,32,"%04x %02x %02x %-10s (I), V0-V%01x", offset, code[0], code[1], "MOVM", lower_nibble);
-	             break;
-	        }
-		case 0x65: {
-                     snprintf(retval,32,"%04x %02x %02x %-10s V0-V%01x, (I)", offset, code[0], code[1], "MOVM", lower_nibble);
-	             break;
-	        }
-		default: {
-		   snprintf(retval,32,"%04x %02x %02x %-10s", offset, code[0], code[1], "UNKNOWN");
-		   break;
-	 	}
-	     }
-	     break;
-	}
-
-
-	default: {
-             snprintf(retval,32,"%04x %02x %02x %-10s", offset, code[0], code[1], "UNKNOWN");
-	}
+void Op0(chip8_cpu_t* cpu, uint8_t* code) {
+     switch(code[1]) {
+         case 0xE0: { // CLS
+              memset(cpu->vram, 0, 64*32);
+	      cpu->registers.PC += 2;
+	      break;
+	 }
+	 case 0xEE: { // RTS
+	      cpu->registers.PC  = *((uint16_t*)&(cpu->ram[cpu->registers.SP]));
+	      cpu->registers.SP += 2;
+	      break;
+	 }
      }
+}
 
-
+void chip8_iter(chip8_cpu_t* cpu) {
+     uint8_t* code = &(cpu->ram[cpu->registers.PC]);
+     uint8_t upper_nibble = UPPER_NIBBLE_U8(code[0]);
+     uint8_t lower_nibble = LOWER_NIBBLE_U8(code[0]);     
+     switch(upper_nibble) {
+        case 0x00: Op0(cpu, code); break;
+/*        case 0x01: Op1(cpu, code); break;
+        case 0x02: Op2(cpu, code); break;
+        case 0x03: Op3(cpu, code); break;
+        case 0x04: Op4(cpu, code); break;
+        case 0x05: Op5(cpu, code); break;
+        case 0x06: Op6(cpu, code); break;
+        case 0x07: Op7(cpu, code); break;
+        case 0x08: Op8(cpu, code); break;
+        case 0x09: Op9(cpu, code); break;
+        case 0x0a: OpA(cpu, code); break;
+        case 0x0b: OpB(cpu, code); break;
+        case 0x0c: OpC(cpu, code); break;
+        case 0x0d: OpD(cpu, code); break;
+        case 0x0e: OpE(cpu, code); break;
+        case 0x0f: OpF(cpu, code); break;*/
+     }
 }
 
 
